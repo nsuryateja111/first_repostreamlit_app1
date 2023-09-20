10e809050b0a8aa70accea268fa46a201e7c1d8d
nsuryateja111 <144252138+nsuryateja111@users.noreply.github.com>
2023-09-20 00:08:44 -0400

    Update streamlit_app.py
    Placing all import commands at top

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit;
 import pandas;
 import requests;
 import snowflake.connector;
-fro urllib.error import URLError;
+from urllib.error import URLError;
 
 #import streamlit
 streamlit.title('My New Menu items')
